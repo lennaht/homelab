eb6a9cfc960a08e26c5615dcaf5bf555b124a073
Lennart Beneke <rulezdzn@gmail.com>
2023-02-04 16:23:08 +0100

    add metallb and test

--- prepare-env.sh
+++ prepare-env.sh
@@ -7,4 +7,9 @@ export SOPS_AGE_KEY=$(cat age.agekey)
 export SOPS_AGE_RECIPIENTS=$(cat age.agekey | grep public | cut -d " " -f4)
 
 export GIT_REPO_URL="ssh://git@github.com/lennaht/homelab"
+
+if [ ! -f "deploy-key" ]
+then
+    ssh-keygen -t rsa -b 4096 -f deploy-key
+fi
 export GIT_PRIVATE_KEYFILE="$(pwd)/deploy-key"
